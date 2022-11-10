13fdb0292c106ba8d673c35eeaaea6b23a2ac4f5
Justin Golden <justingolden21@gmail.com>
2022-11-09 17:02:58 -0800

    White bg for team logo

--- src/routes/[...slugs]/+page.svelte
+++ src/routes/[...slugs]/+page.svelte
@@ -90,7 +90,7 @@
 			<td class="text-xl sm:text-3xl">{ranking.current}</td>
 			<td>
 				<img
-					class="w-6 sm:w-8 md:w-10 mr-2 xs:inline"
+					class="w-6 sm:w-8 md:w-10 mr-2 xs:inline bg-white p-1"
 					loading="lazy"
 					src={ranking.team.logo}
 					alt={ranking.team.name + ' logo'}
